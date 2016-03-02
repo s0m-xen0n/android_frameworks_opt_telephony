8df963b2eb4ff4ae04e9f0e48249082f2ee21922
Wang Xuerui <idontknw.wang@gmail.com>
2016-03-03 01:40:48 +0800

    add previously forgotten prebuilt mtk library
    Change-Id: I12a21326a2122991d5018e4f82a1d75e17e825f3

---
+++ mtk-prebuilts/Simservs/Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := Simservs
+LOCAL_SRC_FILES := javalib.jar
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_MODULE_SUFFIX := .jar
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_PREBUILT)
